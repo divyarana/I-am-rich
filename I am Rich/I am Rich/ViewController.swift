ac30ae7464ddb3a90a6e2a39f038a7db30f60cf8
Divya <divya27rana@gmail.com>
2020-12-10 15:34:04 +0530

    Created a new project called I am Rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Divya Pundir on 12/10/20.
+//  Copyright © 2020 Divya Pundir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
